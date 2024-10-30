573098caac4ec1920b44b95cc6f135e86558870d
melissaceee <cheng.melissa@outlook.com>
2024-10-29 20:31:57 -0400

    update lab 2 exercises 1 2 3

--- exercises.py
+++ exercises.py
@@ -1,1 +1,27 @@
 #exercise 1
+def calculate_area_triangle(base, height):
+    return (base * height) / 2
+
+print('Exercise 1:', calculate_area_triangle(10, 5))
+
+
+
+# Exercise 2: Calculate Simple Interest
+def simple_interest(principal, rate, time):
+    interest = (principal * rate * time) / 100
+    return interest
+
+print('Exercise 2:', simple_interest(1000, 5, 2))
+
+
+
+# Exercise 3: Apply a Discount
+
+def apply_discount(price, discount_percentage):
+    discount_amount = (price * discount_percentage) / 100
+    new_price = price - discount_amount
+    return new_price
+
+
+print('Exercise 3:', apply_discount(100)) 
+
